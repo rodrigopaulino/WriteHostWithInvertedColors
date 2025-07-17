5fee3368f466b60e0075807781f767892d1d9a32
rodrigopaulino <rpfs08@gmail.com>
2025-07-17 19:53:11 +0100

    Adjust for non windows platforms

--- WriteHostWithInvertedColors.psm1
+++ WriteHostWithInvertedColors.psm1
@@ -10,50 +10,36 @@ function Write-Host {
         [Switch]$NoNewLine,
         [String]$Separator,
         [Alias("fg")]
-        [ConsoleColor]$ForegroundColor = [ConsoleColor]::ForegroundColor,
+        [ConsoleColor]$ForegroundColor,
         [Alias("bg")]
-        [ConsoleColor]$BackgroundColor = [ConsoleColor]::BackgroundColor,
+        [ConsoleColor]$BackgroundColor,
         [Alias("i")]
         [Switch]$InvertColors = $False
     )
 
     $InversionMap = @{
-        Black = "White"
-        DarkBlue = "Yellow"
-        DarkGreen = "Magenta"
-        DarkCyan = "Red"
-        DarkRed = "Cyan"
-        DarkMagenta = "Green"
-        DarkYellow = "Blue"
-        Gray = "Black"
-        DarkGray = "White"
-        Blue = "DarkYellow"
-        Green = "DarkMagenta"
-        Cyan = "DarkRed"
-        Red = "DarkCyan"
-        Magenta = "DarkGreen"
-        Yellow = "DarkBlue"
-        White = "Black"
+        Black = "White"; DarkBlue = "Yellow"; DarkGreen = "Magenta"; DarkCyan = "Red"
+        DarkRed = "Cyan"; DarkMagenta = "Green"; DarkYellow = "Blue"; Gray = "Black"
+        DarkGray = "White"; Blue = "DarkYellow"; Green = "DarkMagenta"; Cyan = "DarkRed"
+        Red = "DarkCyan"; Magenta = "DarkGreen"; Yellow = "DarkBlue"; White = "Black"
     }
 
-    if ($InvertColors) {
-        if ($InversionMap.ContainsKey($ForegroundColor.ToString())) {
-            $ForegroundColor = $InversionMap[$ForegroundColor.ToString()]
-        }
+    $IsWinOS = $IsWindows -or $PSVersionTable.Platform -eq 'Win32NT'
 
-        if ($InversionMap.ContainsKey($BackgroundColor.ToString())) {
-            $BackgroundColor = $InversionMap[$BackgroundColor.ToString()]
-        }
+    $ForegroundColor = if ($Null -eq $ForegroundColor) { if ($IsWinOS) { [ConsoleColor]::ForegroundColor } else { "White" } }
+    $BackgroundColor = if ($Null -eq $BackgroundColor) { if ($IsWinOS) { [ConsoleColor]::BackgroundColor } else { "Black" } }
+    if ($InvertColors) {
+        if ($InversionMap.ContainsKey($ForegroundColor.ToString())) { $ForegroundColor = $InversionMap[$ForegroundColor.ToString()] }
+        if ($InversionMap.ContainsKey($BackgroundColor.ToString())) { $BackgroundColor = $InversionMap[$BackgroundColor.ToString()] }
     }
 
     $Arguments = @{}
-    
     if ($Null -ne $Object) { $Arguments['Object'] = $Object }
-    $Arguments['ForegroundColor'] = $ForegroundColor
-    $Arguments['BackgroundColor'] = $BackgroundColor
+    if ($ForegroundColor) { $Arguments['ForegroundColor'] = $ForegroundColor }
+    if ($BackgroundColor) { $Arguments['BackgroundColor'] = $BackgroundColor }
     if ($NoNewLine.IsPresent) { $Arguments['NoNewLine'] = $NoNewLine }
-    if ($Separator -ne $null) { $Arguments['Separator'] = $Separator }
-    
+    if ($Separator -ne $Null) { $Arguments['Separator'] = $Separator }
+
     Microsoft.PowerShell.Utility\Write-Host @Arguments
 }
 
